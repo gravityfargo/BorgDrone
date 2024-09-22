601a841e293370c7cbdea48d6005cc56f715a927
Nathan Price <gravityfargo@gmail.com>
2024-09-22 18:22:12 -0400

    helper types

--- borgdrone/repositories/models.py
+++ borgdrone/repositories/models.py
@@ -5,6 +5,10 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from borgdrone.extensions import db
 
+ListRepository = list["Repository"]
+OptRepository = Optional["Repository"]
+OptListRepository = Optional[ListRepository]
+
 
 class Repository(db.Model):
     __tablename__ = "repository"
