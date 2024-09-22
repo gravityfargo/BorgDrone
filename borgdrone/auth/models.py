f2c252a16cfdf9ab8480899dbd8c041c02b4523c
Nathan Price <gravityfargo@gmail.com>
2024-09-22 18:21:48 -0400

    helper type

--- borgdrone/auth/models.py
+++ borgdrone/auth/models.py
@@ -5,6 +5,8 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from borgdrone.extensions import db
 
+OptUsers = Optional["Users"]
+
 
 class Users(UserMixin, db.Model):
     __tablename__ = "users"
