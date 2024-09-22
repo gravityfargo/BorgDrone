1f2c288b03c299e2ffb88c4894a9211d376063fb
Nathan Price <gravityfargo@gmail.com>
2024-09-22 14:24:22 -0400

    init

---
+++ borgdrone/borg/constants.py
@@ -0,0 +1,35 @@
+# Base commands
+
+BORG_INIT_COMMAND = [
+    "borg --log-json init",
+    "--encryption=KEY",
+    "PATH",
+]
+
+BORG_INFO_COMMAND = [
+    "borg --log-json info --json",
+    "PATH",
+]
+
+BORG_DELETE_COMMAND = [
+    "borg --log-json delete --force",
+    "PATH",
+]
+
+BORG_CREATE_COMMAND = [
+    "borg",
+    "--show-version",
+    "create",
+    "--stats",
+    "--progress",
+    "--one-file-system",
+]
+# --exclude /dir/to/exclude
+# PATH::NAME_FORMAT
+# /dir/to/include
+
+BORG_LIST_COMMAND = [
+    "borg --log-json list --json",
+    "PATH",
+    "--format '{archive}{name}{comment}{id}{tam}{start}{time}{end}{command_line}{hostname}{username}'",
+]
