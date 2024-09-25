5bc4cd8c872ed900ff201b449b75e89db3fd7035
Nathan Price <gravityfargo@gmail.com>
2024-09-25 04:13:57 -0400

    minimized code

--- borgdrone/bundles/BackupDirectoryManager.py
+++ borgdrone/bundles/BackupDirectoryManager.py
@@ -1,99 +1,27 @@
 from sqlalchemy import select
 
 from borgdrone.extensions import db
-from borgdrone.logging import BorgdroneEvent
-from borgdrone.types import OptBool, OptInt, OptStr
+from borgdrone.types import OptInt, OptStr
 
-from .models import BackupBundle, BackupDirectory, OptBackupDirectory
+from .models import BackupDirectory, OptBackupDirectory
 
 
-def get_one(bundledirectory_id: OptInt = None, path: OptStr = None) -> BorgdroneEvent[OptBackupDirectory]:
-    _log = BorgdroneEvent[OptBackupDirectory]()
-    _log.event = "BundleManager.get_one"
-
+def get_one(bundledirectory_id: OptInt = None, path: OptStr = None) -> OptBackupDirectory:
+    stmt = None
     instance = None
 
     if bundledirectory_id:
         stmt = select(BackupDirectory).where(BackupDirectory.id == bundledirectory_id)
-        instance = db.session.scalars(stmt).first()
     elif path:
         stmt = select(BackupDirectory).where(BackupDirectory.path == path)
+
+    if stmt is not None:
         instance = db.session.scalars(stmt).first()
 
-    _log.set_data(instance)
-
-    if not instance:
-        _log.status = "FAILURE"
-        _log.error_message = "BackupDirectory not found."
-    else:
-        _log.status = "SUCCESS"
-        _log.message = "BackupDirectory Retrieved."
-
-    return _log
+    return instance
 
 
-def find_exact_match(**kwargs) -> BorgdroneEvent[OptBackupDirectory]:
-    _log = BorgdroneEvent[OptBackupDirectory]()
-    _log.event = "BundleDirectoryManager.find_exact_match"
-
-    stmt = (
-        select(BackupDirectory)
-        .where(BackupDirectory.path == kwargs["path"])
-        .where(BackupDirectory.permissions == kwargs["permissions"])
-        .where(BackupDirectory.owner == kwargs["owner"])
-        .where(BackupDirectory.group == kwargs["group"])
-        .where(BackupDirectory.exclude == kwargs.get("exclude", False))
-    )
-    match = db.session.scalars(stmt).first()
-
-    _log.set_data(match)
-
-    if not match:
-        _log.error_message = "BackupDirectory not in database."
-    else:
-        _log.message = "BackupDirectory Retrieved."
-
-    return _log
-
-
-def get_or_create_bundledirectory(
-    bundle: BackupBundle, update: OptBool = None, **kwargs
-) -> BorgdroneEvent[OptBackupDirectory]:
-    _event = "BundleDirectoryManager.get_or_create_bundledirectory"
-
-    # Attempt to find an exact match to avoid duplication
-    result_log = find_exact_match(**kwargs)
-    result_log.event = _event
-
-    if result_log.status == "SUCCESS":
-        # BackupDirectory already exists, no need to create a new one
-
-        if update and result_log.data:
-            # Update if requested
-            result_log.data.update(**kwargs)
-            return result_log.return_success("Backup Directory found and updated.")
-
-        # Attach the existing BackupDirectory to the bundle if not already attached
-        if backup_dir := result_log.get_data():
-            if backup_dir not in bundle.backupdirectories:
-                bundle.backupdirectories.append(backup_dir)
-                bundle.commit()  # Commit the association
-                return result_log.return_success("Backup Directory found and attached to bundle.")
-
-    # If no match was found, create a new one
-    result_log = create_bundledirectory(bundle, **kwargs)
-    result_log.event = _event
-
-    if result_log.status == "SUCCESS":
-        return result_log.return_success("Backup Directory created.")
-    else:
-        return result_log.return_failure("Backup Directory could not be created.")
-
-
-def create_bundledirectory(bundle: BackupBundle, **kwargs) -> BorgdroneEvent[OptBackupDirectory]:
-    _log = BorgdroneEvent[OptBackupDirectory]()
-    _log.event = "BundleDirectoryManager.create_bundledirectory"
-
+def create_bundledirectory(**kwargs) -> BackupDirectory:
     # Create a new BackupDirectory instance
     backupdirectory = BackupDirectory()
     backupdirectory.path = kwargs["path"]
@@ -103,16 +31,7 @@ def create_bundledirectory(bundle: BackupBundle, **kwargs) -> BorgdroneEvent[Opt
     backupdirectory.exclude = kwargs["exclude"]
     backupdirectory.commit()
 
-    _log.set_data(backupdirectory)
-
-    if not backupdirectory:
-        _log.status = "FAILURE"
-        _log.error_message = "BackupDirectory could not be created."
-    else:
-        _log.status = "SUCCESS"
-        _log.message = "BackupDirectory created successfully."
-
-    return _log
+    return backupdirectory
 
 
 # def update_bundledirectory(bundle: BackupBundle, **kwargs) -> BorgdroneEvent[OptBackupDirectory]:
